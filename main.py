949921ed784368576e921a5bd520ffa91fa5535e
fanlit <fanlit1998@gmail.com>
2022-03-14 09:19:38 +0800

    Added Number of vertices visited for task 1

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ import Task2
 import Task3
 import json
 
+
 def jsonToDict(filename):
     with open(filename) as json_file:
         dic = json.load(json_file)
     return dic
+
 
 if __name__ == "__main__":
     g = jsonToDict("G.json")
@@ -14,11 +16,11 @@ if __name__ == "__main__":
     cost = jsonToDict("Cost.json")
     coord = jsonToDict("coord.json")
 
-    print("Dijkstra Path:")
+    print("Task 1: Dijkstra Path:")
     Task1.run()
     print()
-    print("Uniform Cost Search Path:")
+    print("Task 2: Uniform Cost Search Path:")
     Task2.ucs(1, 50, 287932, g, dist, cost)
     print()
-    print("A-Star Search Path:")
-    Task3.aStar(1,50, 287932, g, dist, cost, coord)
+    print("Task 3: A-Star Search Path:")
+    Task3.aStar(1, 50, 287932, g, dist, cost, coord)
